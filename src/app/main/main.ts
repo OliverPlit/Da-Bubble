4c5cde05a34729cabe4a100ea24c09b601208bea
Noah Velickovic <noah.velic@hotmail.com>
2025-12-22 23:57:27 +0100

    main flexbox korrektur

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -1,59 +1,73 @@
 import { Component } from '@angular/core';
-import { Header } from "./header/header";
-import { Menu } from "./menu/menu";
+import { Header } from './header/header';
+import { Menu } from './menu/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { ChannelMessages } from './channel-messages/channel-messages';
-import { Threads } from "./threads/threads";
+import { Threads } from './threads/threads';
 import { CommonModule } from '@angular/common';
 import { DirectMessages } from '../main/menu/direct-messages/direct-messages';
 import { ChatDirectMessage } from '../main/menu/chat-direct-message/chat-direct-message';
-import { directMessageContact } from '../main/menu/direct-messages/direct-messages.model'; 
+import { directMessageContact } from '../main/menu/direct-messages/direct-messages.model';
 import { ChatDirectYou } from '../main/menu/chat-direct-you/chat-direct-you';
 import { DirectChatService } from '../services/direct-chat-service';
 import { RouterOutlet } from '@angular/router';
 
-
-
 @Component({
   selector: 'app-main',
-  imports: [Header, RouterOutlet, ChatDirectMessage, Menu, ChannelMessages, DirectMessages, ChatDirectYou, Threads, CommonModule, MatButtonModule, MatSidenavModule],
+  imports: [
+    Header,
+    RouterOutlet,
+    ChatDirectMessage,
+    Menu,
+    ChannelMessages,
+    DirectMessages,
+    ChatDirectYou,
+    Threads,
+    CommonModule,
+    MatButtonModule,
+    MatSidenavModule,
+  ],
   templateUrl: './main.html',
   styleUrl: './main.scss',
 })
 export class Main {
   showNewMessages = false;
   showNewMessagesChat = false;
-    showNewMessagesYou = false
+  showNewMessagesYou = false;
   isMenuOpen = true;
   showThreads = false;
   isChannelMessagesVisible = true;
   selectedChatUser: directMessageContact | null = null;
   activePanel: 'none' | 'chatYou' | 'chatDirect' | 'channel' = 'none';
 
+  constructor(private directChatService: DirectChatService) {}
+  showLeft = true;
+  showRight = true;
 
-constructor(private directChatService: DirectChatService) {}
+  toggleLeft() {
+    this.showLeft = !this.showLeft;
+  }
 
+  toggleRight() {
+    this.showRight = !this.showRight;
+  }
 
+  openChatYou() {
+    this.activePanel = 'chatYou';
+  }
 
+  openChatDirectMessage() {
+    this.activePanel = 'chatDirect';
+  }
 
+  openChannel() {
+    this.activePanel = 'channel';
+  }
 
-openChatYou() {
-  this.activePanel = 'chatYou';
-}
-
-openChatDirectMessage() {
-  this.activePanel = 'chatDirect';
-}
-
-openChannel() {
-  this.activePanel = 'channel';
-}
-
-closePanels() {
-  this.activePanel = 'none';
-}
-
+  closePanels() {
+    this.activePanel = 'none';
+  }
 
   setChannelMessagesVisible(value: boolean) {
     this.isChannelMessagesVisible = value;
@@ -66,14 +80,13 @@ closePanels() {
   toggleNewDirectChat(dm: directMessageContact) {
     console.log('3. Main: Event empfangen!', dm);
     this.selectedChatUser = dm;
-    this.showNewMessagesChat = true; 
+    this.showNewMessagesChat = true;
     console.log('4. Main: Chat geöffnet für:', this.selectedChatUser);
   }
 
-  toggleNewDirectChatYou() { 
+  toggleNewDirectChatYou() {
     console.log('3. Main: You empfangen!');
-    this.showNewMessagesYou = true; 
-    
+    this.showNewMessagesYou = true;
   }
 
   closeNewMessage() {
@@ -81,8 +94,8 @@ closePanels() {
   }
 
   closeDirectChatYou() {
-  this.showNewMessagesYou = false;
-}
+    this.showNewMessagesYou = false;
+  }
 
   get channelMessagesStyle(): { [key: string]: string } {
     const style: { [key: string]: string } = {};
@@ -98,5 +111,4 @@ closePanels() {
   closeDirectChat() {
     this.showNewMessagesChat = false;
   }
-
 }
